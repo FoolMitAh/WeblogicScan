63ec163c1aa24f960c93e585063e6ae161da09ab
MitAh <mitahfool@gmail.com>
2020-04-26 16:51:37 +0800

    autopep8

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@
 
 from .platform import ManageProcessor
 
-def pentest(ip,port):
+
+def pentest(ip, port):
     processor = ManageProcessor()
-    #print(processor.PLUGINS) # {’plugin1': <class '__main__.CleanMarkdownBolds'>}
-    processed = processor.process(ip,port)
-    #processed = processor.process(text="**foo bar**", plugins=('plugin2',))
+    # print(processor.PLUGINS)
+    # {'plugin1': <class '__main__.CleanMarkdownBolds'>}
+    processed = processor.process(ip, port)
+    # processed = processor.process(text="**foo bar**", plugins=('plugin2',))
